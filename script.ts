313ad2055a83100709c6987021be4552899efc70
王勇 <wy19945372694@163.com>
2023-05-08 18:34:29 +0800

    update

--- script.ts
+++ script.ts
@@ -3,7 +3,10 @@ import { PrismaClient } from "@prisma/client"
 const prisma = new PrismaClient()
 
 async function main() {
-    const usersWithPosts = await prisma.user.findMany({
+    const usersWithPosts = await prisma.user.findUnique({
+        where: {
+            email: "wangyong220531@gmail.com"
+        },
         include: {
             posts: true
         }
